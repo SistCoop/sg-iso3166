927ea5d055a16698aa53f85726d2bfe1713ba803
Carlos Feria <carlosthe19916@gmail.com>
2015-03-06 10:38:29 -0500

    initial commit

---
+++ src/ui-iso3166.js
@@ -0,0 +1,301 @@
+(function(){
+
+    var module = angular.module('ui-iso3166', ['restangular']);
+
+    module.constant('CONFIGURATION', {
+        'restUrl': {
+            'iso3166': 'http://localhost:3000'
+        }
+    });
+
+    module.factory('Iso3166Restangular', function(Restangular, CONFIGURATION) {
+        return Restangular.withConfig(function(RestangularConfigurer) {
+            RestangularConfigurer.setBaseUrl(CONFIGURATION.restUrl.iso3166);
+        });
+    });
+
+    module.factory('Iso3166AbstractModel', function(Iso3166Restangular){
+        var url = '';
+        var modelMethos = {
+            $new: function(id){
+                return angular.extend({id: id}, modelMethos);
+            },
+            $build: function(){
+                return angular.extend({id: undefined}, modelMethos, {$save: function(){
+                    return Iso3166Restangular.all(url).post(this);
+                }});
+            },
+            $save: function() {
+                return Iso3166Restangular.one(url, this.id).customPUT(Iso3166Restangular.copy(this),'',{},{});
+            },
+
+            $find: function(id){
+                return Iso3166Restangular.one(url, id).get();
+            },
+            $search: function(queryParams){
+                return Iso3166Restangular.all(url).getList(queryParams);
+            },
+
+            $remove: function(id){
+                return Iso3166Restangular.one(url, id).remove();
+            }
+        }
+    });
+
+    module.factory('CountryCode', function(Iso3166Restangular) {
+
+        var url = 'country_codes';
+        var urlAlpha2Code = 'country_codes/alpha2Code';
+        var urlAlpha3Code = 'country_codes/alpha3Code';
+        var urlNumericCode = 'country_codes/numericCode';
+        var urlCount = 'country_codes/count';
+
+        var modelMethos = {
+            $new: function(id){
+                return angular.extend({id: id}, modelMethos);
+            },
+            $build: function(){
+                return angular.extend({independent: false}, modelMethos, {$save: function(){
+                    return Iso3166Restangular.all(url).post(this);
+                }});
+            },
+            $save: function() {
+                return Iso3166Restangular.one(urlAlpha3Code, this.alpha3Code).customPUT(Iso3166Restangular.copy(this),'',{},{});
+            },
+
+
+            $saveByAlpha2Code: function() {
+                return Iso3166Restangular.one(urlAlpha2Code, this.alpha2Code).customPUT(Iso3166Restangular.copy(this),'',{},{});
+            },
+            $saveByAlpha3Code: function() {
+                return Iso3166Restangular.one(urlAlpha3Code, this.alpha3Code).customPUT(Iso3166Restangular.copy(this),'',{},{});
+            },
+            $saveByNumericCode: function() {
+                return Iso3166Restangular.one(urlNumericCode, this.numericCode).customPUT(Iso3166Restangular.copy(this),'',{},{});
+            },
+
+
+            $find: function(id){
+                return Iso3166Restangular.one(url, id).get();
+            },
+            $search: function(queryParams){
+                return Iso3166Restangular.all(url).getList(queryParams);
+            },
+            $findByAlpha2code: function(alpha2Code){
+                return Iso3166Restangular.one(urlAlpha2Code, alpha2Code).get();
+            },
+            $findByAlpha3code: function(alpha3Code){
+                return Iso3166Restangular.one(urlAlpha3Code, alpha3Code).get();
+            },
+            $findByNumericCode: function(numericCode){
+                return Iso3166Restangular.one(urlNumericCode, numericCode).get();
+            },
+
+
+            $count: function(){
+                return Iso3166Restangular.one(urlCount).get();
+            },
+
+
+            $disable: function(){
+                return Iso3166Restangular.all(url+'/'+this.id+'/disable').post();
+            },
+            $remove: function(id){
+                return Iso3166Restangular.one(urlAlpha3Code, id).remove();
+            },
+            $removeByAlpha2Code: function(id){
+                return Iso3166Restangular.one(urlAlpha2Code, id).remove();
+            },
+            $removeByAlpha3Code: function(id){
+                return Iso3166Restangular.one(urlAlpha3Code, id).remove();
+            },
+            $removeByNumericCode: function(id){
+                return Iso3166Restangular.one(urlNumericCode, id).remove();
+            }
+        };
+
+        Iso3166Restangular.extendModel(url, function(obj) {
+            return angular.extend(obj, modelMethos);
+        });
+        Iso3166Restangular.extendModel(urlAlpha2Code, function(obj) {
+            return angular.extend(obj, modelMethos);
+        });
+        Iso3166Restangular.extendModel(urlAlpha3Code, function(obj) {
+            return angular.extend(obj, modelMethos);
+        });
+        Iso3166Restangular.extendModel(urlNumericCode, function(obj) {
+            return angular.extend(obj, modelMethos);
+        });
+        Iso3166Restangular.extendModel(urlCount, function(obj) {
+            return angular.extend(obj, modelMethos);
+        });
+
+        return modelMethos;
+
+    });
+
+
+   module.factory('CountryName', function(Iso3166Restangular) {
+
+        var url = 'country_names';
+
+        var modelMethos = {
+            $new: function(id){
+                return angular.extend({id: id}, modelMethos);
+            },
+            $build: function(){
+                return angular.extend({id: undefined, independent: false}, modelMethos, {$save: function(){
+                    return Iso3166Restangular.all(url).post(this);
+                }});
+            },
+            $save: function() {
+                return Iso3166Restangular.one(url, this.alpha3Code).customPUT(Iso3166Restangular.copy(this),'',{},{});
+            },
+
+            $find: function(id){
+                return Iso3166Restangular.one(url, id).get();
+            },
+            $search: function(queryParams){
+                return Iso3166Restangular.all(url).getList(queryParams);
+            },
+
+            $disable: function(){
+                return Iso3166Restangular.all(url+'/'+this.id+'/disable').post();
+            },
+            $remove: function(id){
+                return Iso3166Restangular.one(url, id).remove();
+            }
+        };
+
+        Iso3166Restangular.extendModel(url, function(obj) {
+            return angular.extend(obj, modelMethos);
+        });
+
+        return modelMethos;
+
+    });
+
+
+    module.factory('Language', function(Iso3166Restangular) {
+
+        var url = 'languages';
+
+        var modelMethos = {
+            $new: function(id){
+                return angular.extend({id: id}, modelMethos);
+            },
+            $build: function(){
+                return angular.extend({id: undefined, independent: false}, modelMethos, {$save: function(){
+                    return Iso3166Restangular.all(url).post(this);
+                }});
+            },
+            $save: function() {
+                return Iso3166Restangular.one(url, this.alpha3Code).customPUT(Iso3166Restangular.copy(this),'',{},{});
+            },
+
+            $find: function(id){
+                return Iso3166Restangular.one(url, id).get();
+            },
+            $search: function(queryParams){
+                return Iso3166Restangular.all(url).getList(queryParams);
+            },
+
+            $disable: function(){
+                return Iso3166Restangular.all(url+'/'+this.id+'/disable').post();
+            },
+            $remove: function(id){
+                return Iso3166Restangular.one(url, id).remove();
+            }
+        };
+
+        Iso3166Restangular.extendModel(url, function(obj) {
+            return angular.extend(obj, modelMethos);
+        });
+
+        return modelMethos;
+
+    });
+
+
+    module.factory('SubdivisionCategory', function(Iso3166Restangular) {
+
+        var url = 'subdivisionCategories';
+
+        var modelMethos = {
+            $new: function(id){
+                return angular.extend({id: id}, modelMethos);
+            },
+            $build: function(){
+                return angular.extend({id: undefined, independent: false}, modelMethos, {$save: function(){
+                    return Iso3166Restangular.all(url).post(this);
+                }});
+            },
+            $save: function() {
+                return Iso3166Restangular.one(url, this.alpha3Code).customPUT(Iso3166Restangular.copy(this),'',{},{});
+            },
+
+            $find: function(id){
+                return Iso3166Restangular.one(url, id).get();
+            },
+            $search: function(queryParams){
+                return Iso3166Restangular.all(url).getList(queryParams);
+            },
+
+            $disable: function(){
+                return Iso3166Restangular.all(url+'/'+this.id+'/disable').post();
+            },
+            $remove: function(id){
+                return Iso3166Restangular.one(url, id).remove();
+            }
+        };
+
+        Iso3166Restangular.extendModel(url, function(obj) {
+            return angular.extend(obj, modelMethos);
+        });
+
+        return modelMethos;
+
+    });
+
+
+    module.factory('Territory', function(Iso3166Restangular) {
+
+        var url = 'territories';
+
+        var modelMethos = {
+            $new: function(id){
+                return angular.extend({id: id}, modelMethos);
+            },
+            $build: function(){
+                return angular.extend({id: undefined, independent: false}, modelMethos, {$save: function(){
+                    return Iso3166Restangular.all(url).post(this);
+                }});
+            },
+            $save: function() {
+                return Iso3166Restangular.one(url, this.alpha3Code).customPUT(Iso3166Restangular.copy(this),'',{},{});
+            },
+
+            $find: function(id){
+                return Iso3166Restangular.one(url, id).get();
+            },
+            $search: function(queryParams){
+                return Iso3166Restangular.all(url).getList(queryParams);
+            },
+
+            $disable: function(){
+                return Iso3166Restangular.all(url+'/'+this.id+'/disable').post();
+            },
+            $remove: function(id){
+                return Iso3166Restangular.one(url, id).remove();
+            }
+        };
+
+        Iso3166Restangular.extendModel(url, function(obj) {
+            return angular.extend(obj, modelMethos);
+        });
+
+        return modelMethos;
+
+    });
+
+})();
---
+++ test/restangularSpec.js
@@ -0,0 +1,7 @@
+describe("CountryName", function() {
+
+  // Load required modules
+  beforeEach(angular.mock.module("ui-iso3166"));
+
+
+});
